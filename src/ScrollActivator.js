716371b416886d9c2482a0803e8043465db73da2
maecapozzi <maecapozzi@gmail.com>
2018-03-17 15:50:16 -0400

    Set up ScrollActivator component

---
+++ src/ScrollActivator.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class ScrollActivator extends Component {
+  static propTypes = {
+    containerElement: string.PropTypes.isRequired
+  }
+
+  componentDidMount () {}
+}
+
+export default ScrollActivator
