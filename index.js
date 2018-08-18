2011017eba36b24019110d6a5608b6c1b1f7328f
maecapozzi <maecapozzi@gmail.com>
2018-08-18 10:22:35 -0400

    Replace parcel with webpack

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
-import React from "react"
-import { render } from "react-dom"
-
-if (module.hot) {
-  module.hot.accept()
-}
-
-const App = () => <h1>Hi</h1>
-
-render(<App />, document.getElementById("app"))
+import { ScrollActivator } from './src/index'
+export { ScrollActivator }
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'react-scroll-activator.js',
+    library: 'reactScrollActivator',
+    libraryTarget: 'umd',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+}
